f55a3e7a04ec677ce363356cfecb3d297838490d
LoiNV <loinv_c00615@fpt.aptech.ac.vn>
2015-06-10 11:32:38 +0700

    Fix update Tile

--- PivotApp1/PivotApp1/MainPage.xaml.cs
+++ PivotApp1/PivotApp1/MainPage.xaml.cs
@@ -22,10 +22,9 @@ namespace PivotApp1
         {
             InitializeComponent();
 
-            // Set the data context of the listbox control to the sample data
             MainViewModel mvm = new MainViewModel();
-            DataContext = mvm;
-            CreateIconicTile();
+            DataContext = mvm;            
+            
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -33,66 +32,24 @@ namespace PivotApp1
             
         }
 
-        private void CreateIconicTile()
-        {
-            ShellTile tileId = FindTile("CreateIconicTile");
-            if (tileId == null)
-            {
-                FlipTileData flipTile = new FlipTileData
-                {
-                    Title = "Cat Meow",
-                    BackTitle = "ChimPanzee",
-                    WideBackContent = "Một trò chơi vui vẻ và bổ ích cho trẻ em",
-                    SmallBackgroundImage = new Uri("Assets/Images/Animals/CatMeow.jpg", UriKind.RelativeOrAbsolute),
-                    BackgroundImage = new Uri("Assets/Images/Animals/CatMeow.jpg", UriKind.RelativeOrAbsolute),
-                    BackBackgroundImage = new Uri("Assets/Images/Animals/ChimPanzee.jpg", UriKind.RelativeOrAbsolute),
-                    WideBackgroundImage = new Uri("Assets/Images/Animals/CatMeow.jpg", UriKind.RelativeOrAbsolute),
-                    WideBackBackgroundImage = new Uri("Assets/Images/Animals/ChimPanzee.jpg", UriKind.RelativeOrAbsolute)
-                };
-                ShellTile.Create(new Uri("/MainPage.xaml?newflipTile", UriKind.RelativeOrAbsolute), flipTile, true);
-            }
-            
-        }
-
         private void UpdateIconicTile(Items item)
         {
-
-            ShellTile tileId = FindTile("CreateIconicTile");
+            ShellTile tileId = ShellTile.ActiveTiles.First();
             if (tileId != null)
             {
                 FlipTileData flipTile = new FlipTileData
                 {
                     Title = item.Name,
+                    BackTitle = "Game For Kids",
                     SmallBackgroundImage = new Uri(item.ImagePath, UriKind.RelativeOrAbsolute),
                     BackgroundImage = new Uri(item.ImagePath, UriKind.RelativeOrAbsolute),
-                    WideBackgroundImage = new Uri(item.ImagePath, UriKind.RelativeOrAbsolute)
+                    BackBackgroundImage = new Uri("Assets/Tiles/FlipCycleTileMedium.png", UriKind.Relative),
+                    WideBackgroundImage = new Uri(item.ImagePath, UriKind.RelativeOrAbsolute),
+                    WideBackBackgroundImage = new Uri("Assets/Tiles/FlipCycleTileLarge.png", UriKind.Relative),
                 };
-                tileId.Update(flipTile);                
+                tileId.Update(flipTile);
             }
-            else
-            {
-                CreateIconicTile();
-            }
-        }
-        private ShellTile FindTile(String uriArg)
-        {
-            try
-            {
-                ShellTile tileId = ShellTile.ActiveTiles.Single(t =>
-                {
-                    if (t.NavigationUri.ToString().EndsWith(uriArg))
-                    {
-                        return true;
-                    }
-                    return false;
-                });
-                return tileId;
-            }
-            catch
-            {
-                return null;
-            }
-        }
+        }       
 
         private void listAnimals_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
--- PivotApp1/PivotApp1/ViewModels/MainViewModel.cs
+++ PivotApp1/PivotApp1/ViewModels/MainViewModel.cs
@@ -20,28 +20,122 @@ namespace PivotApp1.ViewModels
 
         public MainViewModel()
         {
-            LoadGroup();            
-        }
-        
-        public async Task<List<Items>> LoadItems(string groupName) {            
+            //Animals
+            LoadGroupAnimals();
 
-            var folder = await Package.Current.InstalledLocation.GetFolderAsync("Assets/Images/"+ groupName);
-            var files = await folder.GetFilesAsync();
-            return (files.Select(
-                (f, i) => new Items()
-                {
-                    Name = f.Name,
-                    ImagePath = f.Path,
-                    MediaPath = f.Path.Replace(".jpg", ".wav")
-                }).ToList());
+            //Cartoons
+            LoadGroupCartoons();
+
+            //Taunts
+            LoadGroupTaunts();
+
+            //Warnings
+            LoadGroupWarnings();
+
+            //LoadGroup();
         }
 
-        public async void LoadGroup() {            
-            this.Animals = new Groups("Animals") { Item = await LoadItems("Animals") };
-            this.Cartoons = new Groups("Cartoons") { Item = await LoadItems("Cartoons") };
-            this.Taunts = new Groups("Taunts") { Item = await LoadItems("Taunts") };
-            this.Warnings = new Groups("Warnings") { Item = await LoadItems("Warnings") };
+
+        public void AddItems(List<Items> list, string itemName, string groupName)
+        {
+            Items item = new Items()
+            {
+                Name = itemName,
+                ImagePath = "/Assets/Images/" + groupName + "/" + itemName + ".jpg",
+                MediaPath = "/Assets/Audio/" + groupName + "/" + itemName + ".wav"
+            };
+            list.Add(item);
         }
+
+        public void LoadGroupAnimals()
+        {
+            List<Items> list = new List<Items>();
+            AddItems(list, "CatKitten", "Animals");
+            AddItems(list, "CatMeow", "Animals");
+            AddItems(list, "ChimPanzee", "Animals");
+            AddItems(list, "Cow", "Animals");
+            AddItems(list, "Crickets", "Animals");
+            AddItems(list, "Dog", "Animals");
+            AddItems(list, "Dolphin", "Animals");
+            AddItems(list, "Duck", "Animals");
+            AddItems(list, "HorseGallop", "Animals");
+            AddItems(list, "HorseWalk", "Animals");
+            AddItems(list, "Lion", "Animals");
+            AddItems(list, "Pig", "Animals");
+            AddItems(list, "Rooster", "Animals");
+            AddItems(list, "Sheep", "Animals");
+
+            this.Animals = new Groups("Animals") { Item = list };
+        }
+
+        public void LoadGroupCartoons()
+        {
+            List<Items> list = new List<Items>();
+            AddItems(list, "Boing", "Cartoons");
+            AddItems(list, "Bronk", "Cartoons");
+            AddItems(list, "Buglecharge", "Cartoons");
+            AddItems(list, "Laser", "Cartoons");
+            AddItems(list, "OutHere", "Cartoons");
+            AddItems(list, "Splat", "Cartoons");
+
+            this.Cartoons = new Groups("Cartoons") { Item = list };
+        }
+
+        public void LoadGroupTaunts()
+        {
+            List<Items> list = new List<Items>();
+            AddItems(list, "Cackle", "Taunts");
+            AddItems(list, "ClockTicking", "Taunts");
+            AddItems(list, "Dialup", "Taunts");
+            AddItems(list, "Drumroll", "Taunts");
+            AddItems(list, "ElevatorMusic", "Taunts");
+            AddItems(list, "Laugh-Evil", "Taunts");
+
+            this.Taunts = new Groups("Taunts") { Item = list };
+        }
+
+        public void LoadGroupWarnings()
+        {
+            List<Items> list = new List<Items>();
+            AddItems(list, "Airhorn", "Warnings");
+            AddItems(list, "AirRaid", "Warnings");
+            AddItems(list, "AlarmClock-Bell", "Warnings");
+            AddItems(list, "AlarmClock-Electric", "Warnings");
+            AddItems(list, "Backingup", "Warnings");
+            AddItems(list, "Bell-Church", "Warnings");
+            AddItems(list, "Bell-School", "Warnings");
+            AddItems(list, "Foghorn", "Warnings");
+            AddItems(list, "Glassbreaking", "Warnings");
+            AddItems(list, "MissleAlert", "Warnings");
+            AddItems(list, "Police-UK", "Warnings");
+            AddItems(list, "Police-US", "Warnings");
+            AddItems(list, "Vuvuzela", "Warnings");
+
+            this.Warnings = new Groups("Warnings") { Item = list };
+        }
+
+        //public async Task<List<Items>> LoadItems(string groupName)
+        //{            
+
+        //    var folder = await Package.Current.InstalledLocation.GetFolderAsync("Assets/Images/"+ groupName);
+        //    var files = await folder.GetFilesAsync();
+
+        //    return (files.Select(
+        //        (f, i) => new Items()
+        //        {
+        //            Name = f.Name,
+        //            ImagePath = f.Path,
+        //            MediaPath = f.Path.Replace(".jpg", ".wav")
+        //        }).ToList());
+        //}
+
+        //public async void LoadGroup() { 
+
+        //    this.Animals = new Groups("Animals"){Item = await LoadItems("Animals")};
+        //    this.Cartoons = new Groups("Cartoons") { Item = await LoadItems("Cartoons") };
+        //    this.Taunts = new Groups("Taunts") { Item = await LoadItems("Taunts") };
+        //    this.Warnings = new Groups("Warnings") { Item = await LoadItems("Warnings") };
+        //}
         
     }
 }
